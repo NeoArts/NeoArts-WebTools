7a4081b48f57ecb9c973a452b79df90cef51aa54
Tomas <tparra@unal.edu.co>
2024-09-06 17:48:05 -0500

    Auto stash before checking out "origin/main"

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import vercel from '@astrojs/vercel/serverless';
 
 import react from "@astrojs/react";
 
@@ -7,8 +8,5 @@ import react from "@astrojs/react";
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: 'hybrid',
-  outDir: './docs',
-  build: {
-    assets: 'astro'
-  }
+  adapter: vercel(),
 });
